99c33bf1a8f3f3b8c94b1bf46c91b6fe3c5975b4
ALEXVD14 <alexisvdv2005@gmail.com>
2025-12-19 11:02:28 +0100

    modification + css
    ajout des boutons reserver

--- index.php
+++ index.php
@@ -11,18 +11,15 @@ error_reporting(E_ALL);
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Acceuil Horizon</title>
         <link rel="icon" href="assets/logo.png">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="CSS/style.css">
     </head>
     <body>
         <header>
             <a href="index.php"><h1>Horizon</h1></a>
             <h2>Vos vacances, nous on a la vision!</h2>
             <nav>
-                <a href="index.php">Acceuil</a>
                 <a href="se_connecter.php">Se connecter</a>
             </nav>
-        </header>
-        <main>
             <form method="POST" action="index.php">
                 <p>Planification des vacances:</p>
 
@@ -36,7 +33,8 @@ error_reporting(E_ALL);
 
                 <input type="submit" name="rechercher" value="rechercher">
             </form>
-            
+        </header>
+        <main>
             <?php
             try{
 
@@ -69,11 +67,16 @@ error_reporting(E_ALL);
                     $req = $db->prepare('SELECT * FROM Offre_voyage WHERE id_offre = :offre');
                     $req->execute([':offre'=>$nb_aleatoire_offre]);
                     $reponse_req = $req->fetch();
+                    $offre1 = $reponse_req['img'];
                     
                     ?>
-                    <div class="afficher_offre" id="1">
+                    <div class="afficher_offre" id="offre1">
                         <img src="assets/<?=$reponse_req['img']?>" id="img_presentation" alt="img d'example">
                         <p><?=$reponse_req['description_offre']?></p>
+                        <form action="reservation.php">
+                            <input type="submit" name="reserver_offre1" value="reserver">
+                        </form>
+                        
                     </div>
 
                     <?php
@@ -85,15 +88,20 @@ error_reporting(E_ALL);
                     $req = $db->prepare('SELECT img, description_offre FROM Offre_voyage WHERE id_offre = :offre');
                     $req->execute([':offre'=>$new_nb_aleatoire_offre]);
                     $reponse_req = $req->fetch();
+                    $offre2 = $reponse_req['img'];
                     ?>
 
-                    <div class="afficher_offre" id="2">
+                    <div class="afficher_offre" id="offre2">
                         <img src="assets/<?=$reponse_req['img']?>" id="img_presentation" alt="img d'example">
                         <p><?=$reponse_req['description_offre']?></p>
+                        <form action="reservation.php">
+                            <input type="submit" name="reserver_offre2" value="reserver">
+                        </form>
                     </div>
                     <?php
                     $requete_complete = 0;
                 }
+
                 /*sur base de la recherche,fait un requete preparee*/
                 if($requete_complete == 1){
                     $req = $db->prepare($sql);
@@ -104,7 +112,10 @@ error_reporting(E_ALL);
                     if(isset($rech_via_pays['img'])){
                         echo "<div class='afficher_offre' id='1'>";
                         echo "<img src='assets/",$rech_via_pays['img'],"'alt='img par rapport à la recherche'>";
-                        echo "<p>",$rech_via_pays['description_offre'],"</p>";  
+                        echo "<p>",$rech_via_pays['description_offre'],"</p>";
+                        echo "<form action='reservation.php'>";
+                        echo "<input type='submit' name='reserver_offre1' value='reserver'>";
+                        echo "</form>";
                         echo '</div>';
                     }
                     else{
